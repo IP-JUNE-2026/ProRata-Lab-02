0b0879155cc3ab99076b59fa29413c56d71e412a
IT24100061 <IT24100061@my.sliit.lk>
2026-09-07 17:35:50 +0530

    IT24100061Lab02

---
+++ IT24100061Lab2Q2.java
@@ -0,0 +1,11 @@
+public class IT24100061Lab2Q2 {
+   public static void main (String[] args) {
+   
+     double perimeter = 40.0;
+	 double pi = 3.14;
+	 
+	 double radius = perimeter/(2*pi);
+	 System.out.println("Radius of the circular fence:" + radius);
+	 }
+	}
+	
