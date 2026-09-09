dcd3fd3d3e1859ac55b82a3bc1d7991d1f43e4a7
Kavindu Dayan <it22199058@my.sliit.lk>
2026-09-09 17:30:01 +0530

    Add files via upload

---
+++ Q2/IT22199058Lab2Q2.java
@@ -0,0 +1,9 @@
+public class IT22199058Lab2Q2 {
+  public static void main(String[] args) {
+    double length = 10;
+	double perimeter = 4 * length;
+	double radius = perimeter / (2 * 3.14); // circumference of circle = perimeter of the square
+	
+	System.out.println("Radius of the circular fence: " + radius);
+  }
+}
