7cd8ca47625f87401a34c52ead21f87c083779a0
Abhayabandara.S.R.E.G.S.L. <IT24101871@my.sliit.lk>
2026-09-10 15:00:09 +0530

    Add files via upload

---
+++ Q3/IT24101871Lab2Q3.java
@@ -0,0 +1,17 @@
+public class IT24101871Lab2Q3{
+  
+       public static void main(String[] args) {
+
+        double sideA =3;
+        double sideB =4;
+        double calculation1 =Math.pow(3,2);
+        double calculation2 =Math.pow(4,2);
+        double calculation3 =calculation1 + calculation2;
+        double hypotenuse =Math.pow(calculation3,0.5);
+  
+   System.out.println("Length of the hypotenuse:"+ hypotenuse ) ;
+
+
+    }
+ 
+}
